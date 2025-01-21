d9a4b634ef85ccda71d21f1c0f533d01c779e2f8
koutamanto <katskouta@gmail.com>
2025-01-21 00:39:02 +0000

    レスポンシブ化

--- musicSharingService 2/musicSharingService/musicSharingService 2/musicSharingService/app.py
+++ musicSharingService 2/musicSharingService/musicSharingService 2/musicSharingService/app.py
@@ -73,4 +73,4 @@ def upload():
         return redirect(f"/music/{music_id}")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=80, debug=True)
